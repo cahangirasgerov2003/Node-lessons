c3b58ec2ddea376f03d57f33ed95afd3ae0c1a32
Cahangir_Asgerov <esgerovcahangir12@gmail.com>
2024-03-30 23:23:33 +0400

    With middleware, a static image will be added to the home page

--- lesson-5/app.js
+++ lesson-5/app.js
@@ -9,6 +9,15 @@ const port = 3000;
 
 const hostname = "127.0.0.1";
 
+//MIDDLEWARE USEING HELP USE METHOD
+/* app.use("/test", (req, res, next) => {
+  console.log("Yes");
+
+  next();
+}); */
+
+app.use("/", express.static("public/images"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, `index.html`));
 });
@@ -17,11 +26,15 @@ app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, `about.html`));
 });
 
-app.get("/user/:usersID/book/:bookName", (req, res) => {
+/* app.get("/test", (req, res) => {
+  res.send("This is only test");
+}); */
+
+/* app.get("/user/:usersID/book/:bookName", (req, res) => {
   res.send(`<h3>Kullanici adi : ${req.params.usersID}</h3>
    <p>Book name : ${req.params.bookName}</p>
    `);
-});
+}); */
 
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
